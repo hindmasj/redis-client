03cfb6a07056d5a0c5b7355e5f9d424e60f263fb
hindmasj <hindmasj@users.noreply.github.com>
2022-02-15 17:14:05 +0000

    Bootstrapped config

--- src/main/java/io/github/hindmasj/redis/client/RedisClient.java
+++ src/main/java/io/github/hindmasj/redis/client/RedisClient.java
@@ -3,12 +3,18 @@ package io.github.hindmasj.redis.client;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 public class RedisClient{
 
   private static final Logger logger = LogManager.getLogger();
 
+  private static final Config config = ConfigFactory.load();
+
   public static final void main(String[] argv){
     logger.info("Redis Client Application");
+    logger.info(String.format("Config says url=%s:%d",config.getString("server.host"),config.getInt("server.port")));
   }
 
 }
